c88d521161631e637c7cf336ffb12b5a84dcb5b8
xszhu522 <919317559@qq.com>
2024-03-01 11:49:34 +0000

    Deploying to gh-pages from @ xszhu522/xszhu522.github.io@13d1050ed51199cc3d863b75e1cbae10a57b169a 🚀

---
+++ assets/vidstack-BhYx9Fjk-6tKyu2BB.js
@@ -0,0 +1,1 @@
+import{aG as i,aV as s}from"./vidstack-D1JzjGR7-DlKRI45t.js";class r{constructor(a){this.Ca=a}Bb(){i(this.ca)&&this.Oe()}ra(){s(this.ca)&&window.cancelAnimationFrame(this.ca),this.ca=void 0}Oe(){this.ca=window.requestAnimationFrame(()=>{i(this.ca)||(this.Ca(),this.Oe())})}}export{r as R};
